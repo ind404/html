82cda7d5c9f1b115ecce24bd404ff6f8060fbc90
ind404 <indra.alap@gmail.com>
2016-02-10 14:28:56 -0800

    Testing

---
+++ datatype.java
@@ -0,0 +1,39 @@
+package sample;
+
+public class datatype {
+	public static void main(String args[]) {
+		byte b;
+		short s;
+		int i;
+		long l;
+		float f;
+		double d;
+		char c;
+		boolean b1;
+		String s1;
+		b = 10;
+		s = 30000;
+		i = 999999;
+		l = 123456L;
+		f = 1.23F;
+		d = 123.456789;
+		c = 'K';
+		b1 = true;
+		s1 = "Rama";
+		System.out.println(b);
+		System.out.println("B Value : " + b); // + ====> Concatenation / Joining
+												// Operator
+		System.out.println(b + " is B Value");
+		System.out.println("Ravana has " + b + " Heads");
+		System.out.println("S Value : " + s);
+		System.out.println("I Value : " + i);
+		System.out.println("L Value : " + l);
+		System.out.println("F Value : " + f);
+		System.out.println("D Value : " + d);
+		System.out.println("C Value : " + c);
+		System.out.println("B1 Value : " + b1);
+		System.out.println("S1 Value : " + s1);
+	}
+}
+
+
